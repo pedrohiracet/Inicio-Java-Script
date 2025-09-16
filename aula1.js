ecf4f30ecfcd5d07bd02a663a4e0bc61d3c2bff8
pedrohiracet <pedrohenriquealvesiracet@gmail.com>
2025-09-16 15:37:23 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,39 @@
+/*
+alert('Olá esse é meu site.')
+alert('olá')
+document.writeln('Esse é meu site.')
+console.log('test')
+*/
+a = 3
+b = 8
+c =1.75
+
+
+x = true
+y = false
+
+//alert(a)
+
+tamandua = 123
+oi = 13
+
+pessoa = 'humano'
+Nome = 'Kauã'
+
+x = true
+y = false
+
+//alert(oi*tamandua)
+
+//document.writeln('Seu nome é '+Nome+' e você é um ser '+pessoa)
+
+document.writeln('<h1>A: '+a+' | B: '+b+"</h1>")
+
+document.writeln(
+    'adição: '+(a+b)+
+    '<br>subtração: '+(a-b)+
+    '<br>Multiplicação: '+(a*b)+
+    '<br>divisão: '+(a/b)+
+    '<br>potenciação: '+(a**b)
+
+)

53c9f451a012a03dabb751b373ac08a74c73f448
pedrohiracet <pedrohenriquealvesiracet@gmail.com>
2025-10-14 15:43:59 -0300

    Add files via upload

--- aula3.js
+++ aula3.js
@@ -19,40 +19,77 @@ function somar(){
     n1 = Number(document.getElementById('n1').value)
     n2 = Number(document.getElementById('n2').value)
     soma = n1+n2
-    document.getElementById('resultado').innerHTML = soma
+    document.getElementById('resultadodassomas').innerHTML = soma
 }
 
 function subtrair(){
     n1 = Number(document.getElementById('n1').value)
     n2 = Number(document.getElementById('n2').value)  
     subtracao = n1-n2
-    document.getElementById('resultado').innerHTML = subtracao
+    document.getElementById('resultadodassomas').innerHTML = subtracao
 }
 
 function multiplicar(){
     n1 = Number(document.getElementById('n1').value)
     n2 = Number(document.getElementById('n2').value)  
     multiplicacao = n1*n2
-    document.getElementById('resultado').innerHTML = multiplicacao
+    document.getElementById('resultadodassomas').innerHTML = multiplicacao
 }
 
 function dividir(){
    n1 = Number(document.getElementById('n1').value)
    n2 = Number(document.getElementById('n2').value)
    divisao = n1/n2
-   document.getElementById('resultado').innerHTML = divisao
+   document.getElementById('resultadodassomas').innerHTML = divisao
 }
 
 function potencia(){
     n1 = Number(document.getElementById('n1').value)
     n2 = Number(document.getElementById('n2').value) 
     potenciacao = n1**n2
-    document.getElementById('resultado').innerHTML = potenciacao
+    document.getElementById('resultadodassomas').innerHTML = potenciacao
 }
 
 function porcentagem(){
     n1 = Number(document.getElementById('n1').value)
     n2 = Number(document.getElementById('n2').value) 
     porcento = (n1/100)*n2
-    document.getElementById('resultado').innerHTML = porcento
+    document.getElementById('resultadodassomas').innerHTML = porcento
 }
+
+function resultado(){
+    numero1 = Number(document.getElementById('n1').value)
+    numero2 = Number(document.getElementById('n2').value) 
+    soma = numero1+numero2
+    document.getElementById('resultado').innerHTML += soma+'<br>'
+    subtracao = numero1-numero2
+    document.getElementById('resultado').innerHTML += subtracao+'<br>'
+    multiplicacao = numero1*numero2
+    document.getElementById('resultado').innerHTML += multiplicacao+'<br>'
+    divisao = numero1/numero2
+    document.getElementById('resultado').innerHTML += divisao+'<br>'
+    potenciacao = numero1**numero2
+    document.getElementById('resultado').innerHTML += potenciacao+'<br>'
+    porcento = (numero1*100)/numero2
+    document.getElementById('resultado').innerHTML += porcento+'%<br>'
+}
+function calcular(){
+    number1 = Number(document.getElementById('number1').value)
+    number2 = Number(document.getElementById('number2').value)
+
+    adicao = number1+number2
+    subtracao = number1-number2
+    multiplicacao = number1*number2
+    divisao = number1/number2
+    potenciacao = number1**number2
+    porcento = (number1*100)/number2
+    document.getElementById('calculado').innerHTML =
+    'adição: '+adicao+
+    '<br>subtração: '+subtracao+
+    '<br>multiplicação: '+multiplicacao+
+    '<br>divisão: '+divisao+
+    '<br>potenciação: '+potenciacao+
+    '<br>porcentagem: '+porcento+'%'
+}
+
+

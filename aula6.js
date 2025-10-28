1e8fbf24fb046229b6278861b0c95d25f21a3e7d
pedrohiracet <pedrohenriquealvesiracet@gmail.com>
2025-10-28 14:41:27 -0300

    Add files via upload

--- aula6.js
+++ aula6.js
@@ -1,11 +1,29 @@
 function bichoQueVoceMatou(){
     goblins = Number(document.getElementById('goblins').value)
     esqueleto = Number(document.getElementById('esqueleto').value)
+    gorc = Number(document.getElementById('gorc').value)
     boss = Number(document.getElementById('boss').value)
     goblin = goblins*1
     esqueletopontos = esqueleto*2
+    gorcs = gorc*3
     bosspontos = boss*5
 
-    pontostotal = goblin+esqueletopontos+bosspontos
-    document.getElementById('saidadeinformacao').innerHTML = 'A sua pontuação foi '+pontostotal
-}
+    pontostotal = goblin+esqueletopontos+gorcs+bosspontos
+    document.getElementById('saida').innerHTML = 'A sua pontuação foi '+pontostotal
+
+    if(pontostotal >= 60){
+        document.getElementById('saidadeinformacao').innerHTML = '<b>Sua nota foi: '+'S'+' Você é muito bom.</b>'
+    }else if(pontostotal >= 54){
+        document.getElementById('saidadeinformacao').innerHTML = '<b>Sua nota foi: '+'A'+' Muito bem.</b>'
+    }else if(pontostotal >= 48){
+        document.getElementById('saidadeinformacao').innerHTML = '<b>Sua nota foi: '+'B'+' Você é bom.</b>'
+    }else if(pontostotal >= 42){
+        document.getElementById('saidadeinformacao').innerHTML = '<b>Sua nota foi: '+'C'+'  Melhore sua pontuação.</b>'
+    }else if(pontostotal >= 36){
+        document.getElementById('saidadeinformacao').innerHTML = '<b>Sua nota foi: '+'D'+'  Melhore sua pontuação.</b>'
+    }else if(pontostotal >= 30){
+        document.getElementById('saidadeinformacao').innerHTML = '<b>Sua nota foi: '+'E'+'  Melhore sua pontuação.</b>'
+    }else{
+        document.getElementById('saidadeinformacao').innerHTML = '<b>Sua nota foi: '+'F'+'  Melhore sua pontuação, tente mais vezes, você tem que estudar mais esse jogo para ganhar mais pontos.</b>'
+    }
+    }
